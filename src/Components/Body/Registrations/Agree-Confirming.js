766bea6bf5c18dbafe50764d8b9a0afd36d29ff6
ssvamsee <siddamvamsee@gmail.com>
2023-01-04 18:02:54 +0530

    AC

--- src/Components/Body/Registrations/Agree-Confirming.js
+++ src/Components/Body/Registrations/Agree-Confirming.js
@@ -3,8 +3,9 @@ import { Button, Form, Nav } from "react-bootstrap";
 import PhoneInput from 'react-phone-number-input';
 import camartisans from "../../../Assets/Images/CamArtisans-1.png";
 import 'react-phone-number-input/style.css';
-import signupimg from "../../../Assets/Images/signup.png"
+import ACimg from "../../../Assets/Images/AC.png"
 import { Link } from "react-router-dom";
+import { MDBCheckbox } from 'mdb-react-ui-kit';
 
 function AgreeConfirming(){
     const[value,setValue]=useState();
@@ -14,21 +15,14 @@ function AgreeConfirming(){
                 <div className="hrline"></div>
                 <div className="ACh2"><h2>Almost There!</h2></div>
                 <div className="SUimgcon"><img className="SUimg" src={camartisans} alt="camartisans"/></div>
-                <div className="SUtextcon"><p className="SUtext">Spend Lesstime scheduling,</p><p className="SUtext">More time taking beautiful  memories  </p></div>
+                <div className="ACtextcon"><p className="SUtext">Spend Lesstime scheduling,</p><p className="SUtext">More time taking beautiful  memories  </p></div>
                 {/* <div className="SUsubtextcon"><span className="Susubtext">Welcome to the photographer application process. Let’s begin by getting to know each other. Please provide your mobile number below to start.</span></div> */}
                 <div className="Mobilecon">
                     <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                         {/* <Form.Label className="Formtitlecon"><h2 className="Formtitle">Enter Your Mobile Number</h2></Form.Label> */}
                         {/* <Form.Control type="number" /> */}
-                        <PhoneInput
-                            international
-                            countryCallingCodeEditable={false}
-                            defaultCountry="IN"
-                            initialValueFormat="national"
-                            value={value}
-                            onChange={setValue}
-                            className="inputfield"/>
+                        <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' className="Agreebox" /><div className="Agreetextcon"><h5 className="Agreetext">I Agree</h5></div>
                         <Form.Text className="text-muted">By creating an account, You agree to <span>CamArtisans</span> terms and conditions</Form.Text>
                         </Form.Group>
                     </Form>
@@ -36,7 +30,7 @@ function AgreeConfirming(){
                 </div>
                 <div className="SUbuttoncon"><Nav.Link className="SUbut" as={Link} to="/Registered"><Button className="SUbutton">CONFIRM</Button></Nav.Link></div>
                 <div className="Rightlg"></div>
-                <div className="SUimgcon1"><img className="SUimg1" src={signupimg} alt="Signup Image"/></div>
+                <div className="acimgcon1"><img className="SUimg1" src={ACimg} alt="Signup Image"/></div>
             </div>
         </>
     );
